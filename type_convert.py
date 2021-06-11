39d5b034ab644f59d921ed24ba8ea8cbbcc4f055
Free_mxb <13675135392@126.com>
2021-06-11 21:56:10 +0800

    Add type convert usage

---
+++ type_convert.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Description     :Usage of type covert
+@Date     :2021/06/11 15:44:40
+@Author      :xbMa
+'''
+# str -> bytes
+s = "hello world"
+b = s.encode()
+b1 = b"hello world"
+
+# bytes -> hex
+h = b.hex()
+
+# hex -> bytes
+b2 = bytes.fromhex(h)
+
+# int -> hex
+i = 55
+h1 = hex(i)
+
+# int -> bytes
+b3 = str(i).encode()
+b4 = b"55"
+
+# from_bytes
+'''
+    int.from_bytes() 功能是将字节转化成int型数字,'12'如果没有标明进制，看做ascii码值;
+    '1' = 49 = 0011 0001, '2' = 50 = 0011 0010，如果byteorder = 'big', b'12' = 0011 0001 0011 0010 = 12594；
+    如果byteorder = 'littlele', b'12' = 0011 0010 0011 0001 = 12849。第三个参数为signed表示有符号和无符号；
+'''
+i1 = int.from_bytes(b"12", byteorder="big")
+i2 = int.from_bytes(b"12", byteorder="little")
+
+# to_bytes
+'''
+    (number).to_bytes()功能将整数转化成byte
+'''
+b5 = (22).to_bytes(2, byteorder='big')
