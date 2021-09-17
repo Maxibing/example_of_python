5659432b2b66105f9ec0c7e381b4f58eed504550
xbMa <xbMa@nurlink.com>
2021-09-17 10:07:49 +0800

    Add usage of sorted

---
+++ sorted_demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Description     :Usage of sorted()
+@Date     :2021/09/17 10:00:56
+@Author      :xbMa
+'''
+
+n = [56,58,4,6,68,4,18,6,5]
+
+s = sorted(n)
+_s = sorted(n, reverse=True)
+print(n)
+print(s)
+print(_s)
